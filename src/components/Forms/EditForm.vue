30c79e2f12a18f8587cdc73538fc4c5f94e2ec3a
ibrokhim <ibo-707@mail.ru>
2021-12-07 14:33:04 +0500

    commit

---
+++ src/components/Forms/EditForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <n-form>
+    <n-space vertical>
+      <n-space>
+        <n-form-item label="Post ID">
+          <n-input
+            type="number"
+            v-model:value="postData.id"
+            :disabled="false"
+          />
+        </n-form-item>
+        <n-form-item label="User ID">
+          <n-input
+            type="number"
+            v-model:value="postData.userId"
+            :disabled="false"
+          />
+        </n-form-item>
+      </n-space>
+        <n-form-item label="Title">
+          <n-input
+            type="text"
+            v-model:value="postData.title"
+            :disabled="false"
+          />
+        </n-form-item>
+        <n-form-item label="Body">
+          <n-input
+            type="textarea"
+            v-model:value="postData.body"
+            :disabled="false"
+          />
+        </n-form-item>
+    </n-space>
+  </n-form>
+</template>
+
+<script>
+export default {
+  name: "EditForm",
+  props: ["postData"],
+  mounted() {
+
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/UI/Navbar/Navbar.vue
@@ -0,0 +1,178 @@
+<template>
+  <n-layout-header :inverted="$store.state.inverted" bordered>
+    <f-nav-bar :navbarOptions="items" />
+  </n-layout-header>
+</template>
+
+<script>
+import { h, resolveComponent } from "vue";
+import { NIcon } from "naive-ui";
+import {
+  BookOutline as BookIcon,
+  Cog as UserCog,
+  LanguageSharp,
+  LogOut,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+} from "@vicons/ionicons5";
+import { useStore } from 'vuex'
+
+export default {
+  mounted() {},
+  methods: {
+
+  },
+  setup() {
+    let store = useStore()
+    const renderIcon = (icon) => {
+      return () => h(NIcon, null, { default: () => h(icon) });
+    }
+    return {
+      items: [
+        {
+          label: () =>
+            h(
+              resolveComponent("router-link"),
+              {
+                to: {
+                  name: "CategoriesMain",
+                },
+              },
+              { default: () => "About" }
+            ),
+          key: "hear-the-wind-sing",
+          icon: renderIcon(BookIcon),
+        },
+        {
+          label: () =>
+            h(
+              resolveComponent("router-link"),
+              {
+                to: {
+                  name: "CategoriesMain",
+                },
+              },
+              { default: () => "Список" }
+            ),
+          key: "pinball-1973",
+          icon: renderIcon(BookIcon),
+          disabled: true,
+          children: [
+            {
+              label: "Rat",
+              key: "rat",
+            },
+          ],
+        },
+        {
+          label: "A Wild Sheep Chase",
+          key: "a-wild-sheep-chase",
+          disabled: true,
+          icon: renderIcon(BookIcon),
+        },
+        {
+          label: "Dance Dance Dance",
+          key: "Dance Dance Dance",
+          icon: renderIcon(BookIcon),
+          children: [
+            {
+              type: "group",
+              label: "People",
+              key: "people",
+              children: [
+                {
+                  label: "Narrator",
+                  key: "narrator",
+                  icon: renderIcon(PersonIcon),
+                },
+                {
+                  label: "Sheep Man",
+                  key: "sheep-man",
+                  icon: renderIcon(PersonIcon),
+                },
+              ],
+            },
+            {
+              label: "Beverage",
+              key: "beverage",
+              icon: renderIcon(WineIcon),
+              children: [
+                {
+                  label: "Whisky",
+                  key: "whisky",
+                },
+              ],
+            },
+            {
+              label: "Food",
+              key: "food",
+              children: [
+                {
+                  label: "Sandwich",
+                  key: "sandwich",
+                },
+              ],
+            },
+            {
+              label: "The past increases. The future recedes.",
+              key: "the-past-increases-the-future-recedes",
+            },
+          ],
+        },
+        {
+          label: "Profile",
+          key: "Profile",
+          icon: renderIcon(UserCog),
+          children: [
+            {
+              type: "group",
+              label: "People",
+              key: "people",
+            },
+            {
+              label: "The past increases. The future recedes.",
+              key: "the-past-increases-the-future-recedes",
+            },
+          ],
+        },
+        {
+          label: store.state.language.locale,
+          key: "Language",
+          icon: renderIcon(LanguageSharp),
+          children: [
+            {
+              label: "Ru",
+              key: "ru",
+              icon: renderIcon(LanguageSharp),
+              onClick: () => {
+                store.state.language.locale = "RU";
+              },
+            },
+            {
+              label: "Uz",
+              key: "uz",
+              icon: renderIcon(LanguageSharp),
+              onClick: () => {
+                store.state.language.locale = "UZ";
+              },
+            },
+          ],
+        },
+        {
+          label: () =>
+            h(resolveComponent("router-link"), {
+              to: {
+                name: "SignIn",
+              },
+            }),
+          key: "SignIn",
+          icon: renderIcon(LogOut),
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
